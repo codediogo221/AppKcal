2d8f9655e44836fbaac6368be3381ccdc9db7806
Diogo Santos <diogops.estudos@gmail.com>
2023-11-18 15:37:30 -0300

    resolve

--- src/screens/Metas/Metas.js
+++ src/screens/Metas/Metas.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { FlatList } from 'react-native';
 import * as Styles from './style';
-import { metas as initialMetas } from '../../components/mocks'
+import { metas as initialMetas } from '../../components/mocks/metas'
 import Icon from 'react-native-vector-icons/FontAwesome';
 
 export default function Metas({ navigation }) {
